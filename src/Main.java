cd153def3a15ffbdafd97fb3ca64e418e19a534e
brianjayaandhika <brianjaya.andhika@gmail.com>
2025-11-03 22:34:43 +0700

    update deleteByValue, handle if data not found and display if list empty

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,47 @@
 
 public class Main {
     public static void main(String[] args) {
-        // Jawaban Pertanyaan di line 36 ~ 56
+        // Jawaban Pertanyaan di bagian bawah.
 
-        Node head = new Node(15);
+        Node head = new Node(1);
         SingleLinkedList singleLinkedList = new SingleLinkedList(head);
 
-        singleLinkedList.insertAtEnd(25);
-        singleLinkedList.insertAtEnd(35);
-
+        singleLinkedList.insertAtEnd(2);
+        singleLinkedList.insertAtBeginning(4);
+        singleLinkedList.insertAtEnd(3);
         singleLinkedList.insertAtBeginning(5);
-        singleLinkedList.insertAtEnd(45);
 
+        singleLinkedList.insertAtEnd(6);
+        singleLinkedList.insertAtEnd(7);
+        singleLinkedList.insertAtBeginning(8);
+        singleLinkedList.deleteByValue(8); // Delete value at head
+        singleLinkedList.deleteByValue(7); // Delete value at end
+        singleLinkedList.deleteByValue(4); // Delete value at middle
 
-
-        singleLinkedList.insertAtBeginning(1);
-        singleLinkedList.deleteByValue(45);
         singleLinkedList.display();
 
-        // Output
-        //1
-        //->
-        //5
-        //->
-        //15
-        //->
-        //25
-        //->
-        //35
-        //->
     }
 
 
-    // Pertanyaan:
-    // 1. Bagaimana memori untuk list dialokasikan dan dikelola dalam metode createList?
-    // 2. Apa yang akan terjadi pada memori yang dialokasikan untuk list setelah metode
-    //    createList selesai dieksekusi?
-    // 3. Apakah ada potensi kebocoran memori dalam kode di atas? Jelaskan jawaban Anda.
+        // Pertanyaan:
+        // 1. Bagaimana memori untuk list dialokasikan dan dikelola dalam metode createList?
+        // 2. Apa yang akan terjadi pada memori yang dialokasikan untuk list setelah metode
+        //    createList selesai dieksekusi?
+        // 3. Apakah ada potensi kebocoran memori dalam kode di atas? Jelaskan jawaban Anda.
 
-    // Jawaban
-    // 1. Saya kurang tau createList disini mengacu pada apa, tapi saya akan asumsikan, createList adalah sebuah metode yang akan me-return sebuah List.
-    // Maka, variabel tempat menyimpan hasil dari method tersebut, akan disimpan di dalam MEMORY STACK, kemudian, untuk list yang merupakan hasil dari
-    // method tersebut, akan disimpan di dalam MEMORY HEAP. Untuk variabel dan object yang dibuat di dalam method createList, akan disimpan di dalam suatu
-    // memory frame stack baru, yang akan dibebaskan kembali setelah method itu selesai dipanggil.
+        // Jawaban
+        // 1. Saya kurang tau createList disini mengacu pada apa, tapi saya akan asumsikan, createList adalah sebuah metode yang akan me-return sebuah List.
+        // Maka, variabel tempat menyimpan hasil dari method tersebut, akan disimpan di dalam MEMORY STACK, kemudian, untuk list yang merupakan hasil dari
+        // method tersebut, akan disimpan di dalam MEMORY HEAP. Untuk variabel dan object yang dibuat di dalam method createList, akan disimpan di dalam suatu
+        // memory frame stack baru, yang akan dibebaskan kembali setelah method itu selesai dipanggil.
 
-    // 2. Yang akan terjadi pada memori yang dialokasikan untuk list setelah metode createList selesai dieksekusi, untuk variabel dan object yang
-    // dideklarasikan dan berada di scope local method tersebut, maka akan dibebaskan memorinya oleh Garbage Collector apabila memang sudah tidak
-    // bisa dijangkau atau tidak lagi dipakai. Namun untuk variabel yang menampung hasil dan hasil dari method itu sendiri, akan tetap disimpan di memori.
+        // 2. Yang akan terjadi pada memori yang dialokasikan untuk list setelah metode createList selesai dieksekusi, untuk variabel dan object yang
+        // dideklarasikan dan berada di scope local method tersebut, maka akan dibebaskan memorinya oleh Garbage Collector apabila memang sudah tidak
+        // bisa dijangkau atau tidak lagi dipakai. Namun untuk variabel yang menampung hasil dan hasil dari method itu sendiri, akan tetap disimpan di memori.
 
-    // 3. Menurut saya tidak ada, karena, semua proses kalkulasi, looping, pembuatan hashmap, peng-inputan data ke dalam hashmap, semua dilakukan di dalam
-    // method PairSum, yang hanya akan me-return value yang di deklarasikan di dalam method tersebut, sehingga, apabila memang, hashmap di dalam method
-    // merupakan siklik referens, hal tersebut juga tidak akan menyebabkan memory leak, karena setelah method selesai dipanggil dan memberikan hasil,
-    // maka memori variabel dan object yang ada di dalam method tersebut akan dibebaskan oleh Garbage Collector. Jadi untuk contoh kode di atas, yang akan tersimpan
-    // di memori hanyalah variabel A dan k di dalam memori stack.
-}
+        // 3. Menurut saya tidak ada, karena, semua proses kalkulasi, looping, pembuatan hashmap, peng-inputan data ke dalam hashmap, semua dilakukan di dalam
+        // method PairSum, yang hanya akan me-return value yang di deklarasikan di dalam method tersebut, sehingga, apabila memang, hashmap di dalam method
+        // merupakan siklik referens, hal tersebut juga tidak akan menyebabkan memory leak, karena setelah method selesai dipanggil dan memberikan hasil,
+        // maka memori variabel dan object yang ada di dalam method tersebut akan dibebaskan oleh Garbage Collector. Jadi untuk contoh kode di atas, yang akan tersimpan
+        // di memori hanyalah variabel A dan k di dalam memori stack.
+    }
--- src/SingleLinkedList.java
+++ src/SingleLinkedList.java
@@ -26,26 +26,44 @@ public class SingleLinkedList {
     }
 
     public void deleteByValue(int data) {
+        if (this.head.getData() == data) {
+            this.head = this.head.next;
+            return;
+        }
+
         Node current = this.head;
         Node prev = null;
+
         while (true) {
+            if (current == null) {
+                System.out.println("Value not found");
+                break;
+            }
+
             if (current.getData() == data) {
                 if (current.next == null) {
                     prev.next = null;
                 } else {
-                    current = current.next.getNext();
+                    prev.next = current.next;
                 }
+
                 break;
             }
-            prev = current;
-            current = current.getNext();
-        }
 
+            prev = current;
+            current = current.next;
+        }
     }
 
     public void display() {
         System.out.println("======================= Running Display =======================");
         Node current = this.head;
+
+        if(current == null){
+            System.out.println("List is empty");
+            return;
+        }
+
         while (current != null) {
             System.out.println(current.getData());
             System.out.println("->");
